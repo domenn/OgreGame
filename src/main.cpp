bed253d053f3c632f6628f02b12d6654b4a2aa58
Domen Mori <domen.mory@gmail.com>
2020-11-09 18:18:13 +0100

    It compiles

---
+++ src/main.cpp
@@ -0,0 +1,91 @@
+#include "Ogre.h"
+#include "OgreApplicationContext.h"
+#include <iostream>
+
+class MyTestApp : public OgreBites::ApplicationContext, public OgreBites::InputListener
+{
+public:
+    MyTestApp();
+    void setup();
+    bool keyPressed(const OgreBites::KeyboardEvent& evt);
+};
+
+//! [constructor]
+MyTestApp::MyTestApp() : OgreBites::ApplicationContext()
+{
+}
+//! [constructor]
+
+//! [key_handler]
+bool MyTestApp::keyPressed(const OgreBites::KeyboardEvent& evt)
+{
+    if (evt.keysym.sym == OgreBites::SDLK_ESCAPE)
+    {
+        getRoot()->queueEndRendering();
+    }
+    return true;
+}
+//! [key_handler]
+
+//! [setup]
+void MyTestApp::setup(void)
+{
+    // do not forget to call the base first
+    OgreBites::ApplicationContext::setup();
+    
+    // register for input events
+    addInputListener(this);
+
+    // get a pointer to the already created root
+    Ogre::Root* root = getRoot();
+    Ogre::SceneManager* scnMgr = root->createSceneManager();
+
+    // register our scene with the RTSS
+    Ogre::RTShader::ShaderGenerator* shadergen = Ogre::RTShader::ShaderGenerator::getSingletonPtr();
+    shadergen->addSceneManager(scnMgr);
+
+    // without light we would just get a black screen    
+    Ogre::Light* light = scnMgr->createLight("MainLight");
+    Ogre::SceneNode* lightNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    lightNode->setPosition(0, 10, 15);
+    lightNode->attachObject(light);
+
+    // also need to tell where we are
+    Ogre::SceneNode* camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    camNode->setPosition(0, 0, 15);
+    camNode->lookAt(Ogre::Vector3(0, 0, -1), Ogre::Node::TS_PARENT);
+
+    // create the camera
+    Ogre::Camera* cam = scnMgr->createCamera("myCam");
+    cam->setNearClipDistance(5); // specific to this sample
+    cam->setAutoAspectRatio(true);
+    camNode->attachObject(cam);
+
+    // and tell it to render into the main window
+    getRenderWindow()->addViewport(cam);
+
+    // finally something to render
+    Ogre::Entity* ent = scnMgr->createEntity("Sinbad.mesh");
+    Ogre::SceneNode* node = scnMgr->getRootSceneNode()->createChildSceneNode();
+    node->attachObject(ent);
+}
+//! [setup]
+
+//! [main]
+int main(int argc, char *argv[])
+{
+	//try
+	//{
+				    MyTestApp app;
+    app.initApp();
+    app.getRoot()->startRendering();
+    app.closeApp();
+	//}
+	//catch (std::exception const & stde)
+	//{
+ //       std::cout << "Exception ... " << stde.what();
+	//}
+
+    return 0;
+}
+//! [main]
